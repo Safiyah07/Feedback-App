cfbba276c28f9bfb08c6b917331e19aaa735ba1e
Safiyah <safiyahmasud@gmail.com>
2023-02-17 14:19:43 +0100

    framer motion fro feedback animation

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
 import FeedbackItem from './FeedbackItem'
 import FeedbackContext from '../context/FeedbackContext'
 import Spinner from './shared/Spinner'
@@ -14,12 +15,21 @@ function FeedbackList() {
 		<Spinner />
 	) : (
 		<div>
-			{feedback.map((item) => (
-				<FeedbackItem
-					key={item.id}
-					item={item}
-				/>
-			))}
+			<AnimatePresence>
+				{feedback.map((item) => (
+					<motion.div
+						key={item.id}
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						exit={{ opacity: 0 }}
+					>
+						<FeedbackItem
+							key={item.id}
+							item={item}
+						/>
+					</motion.div>
+				))}
+			</AnimatePresence>
 		</div>
 	)
 
